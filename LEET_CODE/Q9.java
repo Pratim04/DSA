d8e14893cc8c12fed79928666781b01358db1f19
Pratim <12345678+Pratim04@users.noreply.github.com>
2026-02-01 16:34:41 +0530

    DELETE TEMP FILE

---
+++ LEET_CODE/Q9.java
@@ -0,0 +1,11 @@
+package LEET_CODE;
+// Palindrome of a given number
+public class Q9 {
+    public static void main(String[] args) {
+        
+    }
+
+    public static void Palindrome(int x) {
+        
+    }
+}
