5b9bed46e380ca68bf1864ca678c87ff0c0eb942
Pratim <12345678+Pratim04@users.noreply.github.com>
2026-02-03 16:28:09 +0530

    AMAZON INV QUESTION

---
+++ COMPANY-QNS/QAMZ.java
@@ -0,0 +1,47 @@
+// Find Position of an element in a sorted
+// array of infinite numbers
+// Question link : https://www.geeksforgeeks.org/dsa/find-position-element-sorted-array-infinite-numbers
+public class QAMZ {
+    public static void main(String[] args) {
+        int[] arr = { 3, 5, 7, 9, 10, 90, 100,
+                130, 140, 160, 170 };
+        int target = 10;
+        int result = ans(arr, target);
+        System.out.println(result + 1);
+    }
+
+    static int ans(int[] arr, int target) {
+        // first find the range
+        // first start with a box of size 2
+        int start = 0;
+        int end = 1;
+
+        // conditon for the target to lie in the range
+        while (target > arr[end]) {
+            int temp = end + 1;
+            // double the box value
+            // end = previous end + sizeOfbox*2
+            end = end + (end - start + 1) * 2;
+            start = temp;
+        }
+        return BinarySearch(arr, target, start, end);
+    }
+
+    public static int BinarySearch(int[] arr, int target, int start, int end) {
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target>arr[mid]) {
+                start = mid + 1;
+            }
+            else if (target<arr[mid]) {
+                end = mid - 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
