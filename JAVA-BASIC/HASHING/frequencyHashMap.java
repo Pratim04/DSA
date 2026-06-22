33afdc473726fcd7aeae2f28d6a8db8daec13233
Pratim <pratimhalder04@gmail.com>
2026-06-22 15:31:37 +0530

    changes

--- JAVA-BASIC/HASHING/frequencyHashMap.java
+++ JAVA-BASIC/HASHING/frequencyHashMap.java
@@ -1,6 +1,7 @@
 package HASHING;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class frequencyHashMap {
 
@@ -16,6 +17,31 @@ public class frequencyHashMap {
     //     System.out.println(map);
     // }
 
-    
+    public void Frequency(int[] arr, int n) {
+        // Create a HashMap to store frequency of each element
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Traverse the array and count frequencies
+        for (int i = 0; i < n; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        // Traverse through the HashMap and print frequencies
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        // Input array
+        int[] arr = {10, 5, 10, 15, 10, 5};
+        int n = arr.length;
+
+        // Create Solution instance
+        Solution sol = new Solution();
+
+        // Call the function to count frequencies
+        sol.Frequency(arr, n);
+    }
     
 }
