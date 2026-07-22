24b5c7090a2f5cb454a1563dfe4272023c48b519
Pratim <pratimhalder04@gmail.com>
2026-07-22 21:58:41 +0530

    sorting added

---
+++ SORTING/InsertionSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {13, 46, 24, 52, 20, 9};
+
+        InsertionSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static int[] InsertionSort(int[] arr) {
+        for(int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while(j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+        
+        return arr;
+    }
+}
